fe78a48644697d0253d69d7863b721c130d727f6
bella <theboss@ciaobella.me>
2021-06-10 13:52:54 -0700

    trial 1

---
+++ newcop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+
+
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double(Math.PI * radius * radius);
+
+        }
+        
+
+
+    }
+}
